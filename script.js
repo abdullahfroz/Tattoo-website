ad0dfdc8f4c09345372722c860dc8b62114ffe23
abdullahfroz <abdullahferoz93@gmail.com>
2026-08-09 17:55:53 +0200

    logo3

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-const logo = document.getElementById("rotating-logo");
+document.addEventListener("DOMContentLoaded", () => {
 
-let targetRotation = 0;
-let currentRotation = 0;
+    const logo = document.getElementById("rotating-logo");
 
-window.addEventListener("scroll", () => {
-    targetRotation = window.scrollY * 0.08;
+    if (!logo) {
+        console.log("Logo not found");
+        return;
+    }
+
+    let currentRotation = 0;
+    let targetRotation = 0;
+
+    window.addEventListener("scroll", () => {
+
+        targetRotation = window.scrollY * 0.08;
+
+    });
+
+    function animateLogo() {
+
+        currentRotation +=
+            (targetRotation - currentRotation) * 0.05;
+
+        logo.style.transform =
+            `rotate(${currentRotation}deg)`;
+
+        requestAnimationFrame(animateLogo);
+    }
+
+    animateLogo();
+
 });
-
-function animateLogo() {
-    currentRotation += (targetRotation - currentRotation) * 0.05;
-
-    logo.style.transform = `rotate(${currentRotation}deg)`;
-
-    requestAnimationFrame(animateLogo);
-}
-
-animateLogo();
