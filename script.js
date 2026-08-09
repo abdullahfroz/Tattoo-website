f31e2a09e068a493eeab075f178abe7372e1689c
abdullahfroz <abdullahferoz93@gmail.com>
2026-08-09 15:51:53 +0200

    motion

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const logo = document.getElementById("rotating-logo");
 
+let targetRotation = 0;
+let currentRotation = 0;
+
 window.addEventListener("scroll", () => {
 
     const scrollPosition = window.scrollY;
 
-   const rotation = scrollPosition * 0.08;
-
-    logo.style.transform = `rotate(${rotation}deg)`;
+    targetRotation = scrollPosition * 0.08;
 
 });
+
+
+function animateLogo() {
+
+    currentRotation += (targetRotation - currentRotation) * 0.08;
+
+    logo.style.transform = `rotate(${currentRotation}deg)`;
+
+    requestAnimationFrame(animateLogo);
+
+}
+
+
+animateLogo();
